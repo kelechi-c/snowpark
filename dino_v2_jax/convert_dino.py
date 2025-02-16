91763f2a35f10a6ca0f35e67097a315ca4da7409
kelechi-c <narutokelechi@gmail.com>
2025-02-16 06:29:11 +0100

    [placing] move layer conversion utils

--- dino_v2_jax/convert_dino.py
+++ dino_v2_jax/convert_dino.py
@@ -10,68 +10,12 @@ from jax import Array
 import numpy as np
 from dinov2 import DinoViT
 from reference.hfmodels.dinov2.modeling_dinov2 import Dinov2Model
-
-
-def torch_embedding_to_jax_embedding(torch_embedding: torch.nn.Embedding) -> nnx.Embed:
-    jax_embedding: nnx.Embed = nnx.eval_shape(
-        lambda: nnx.Embed(
-            num_embeddings=torch_embedding.num_embeddings,
-            features=torch_embedding.embedding_dim,
-            rngs=nnx.Rngs(0),
-        )
-    )
-    jax_embedding.embedding.value = jnp.array(torch_embedding.weight.detach().numpy())
-    return jax_embedding
-
-
-def torch_linear_to_jax_linear(torch_linear: torch.nn.Linear) -> nnx.Linear:
-    dense: nnx.Linear = nnx.eval_shape(
-        lambda: nnx.Linear(
-            in_features=torch_linear.in_features,
-            out_features=torch_linear.out_features,
-            use_bias=torch_linear.bias is not None,
-            rngs=nnx.Rngs(0),
-        )
-    )
-    dense.kernel.value = jnp.array(torch_linear.weight.T.detach().numpy())
-    if torch_linear.bias is not None:
-        dense.bias.value = jnp.array(torch_linear.bias.detach().numpy())
-    return dense
-
-
-def torch_layernorm_to_jax_layernorm(
-    torch_layernorm: torch.nn.LayerNorm,
-) -> nnx.LayerNorm:
-    jax_layernorm: nnx.LayerNorm = nnx.eval_shape(
-        lambda: nnx.LayerNorm(
-            num_features=torch_layernorm.normalized_shape[0],
-            epsilon=torch_layernorm.eps,
-            use_bias=torch_layernorm.bias is not None,
-            use_scale=torch_layernorm.weight is not None,
-            rngs=nnx.Rngs(0),
-        )
-    )
-    if torch_layernorm.weight is not None:
-        jax_layernorm.scale.value = jnp.array(torch_layernorm.weight.detach().numpy())
-    if torch_layernorm.bias is not None:
-        jax_layernorm.bias.value = jnp.array(torch_layernorm.bias.detach().numpy())
-    return jax_layernorm
-
-
-def torch_conv_to_jax_conv(torch_conv: torch.nn.Conv2d) -> nnx.Conv:
-    dense: nnx.Linear = nnx.eval_shape(
-        lambda: nnx.Linear(
-            in_features=torch_conv.in_channels,
-            out_features=torch_conv.out_channels,
-            use_bias=torch_conv.bias is not None,
-            rngs=nnx.Rngs(0),
-        )
-    )
-    res_weight = rearrange(torch_conv.weight, "d c h w -> h w c d")
-    dense.kernel.value = jnp.array(res_weight.detach().numpy())
-    if torch_conv.bias is not None:
-        dense.bias.value = jnp.array(torch_conv.bias.detach().numpy())
-    return dense
+from ..utils import (
+    torch_embedding_to_jax_embedding,
+    torch_layernorm_to_jax_layernorm,
+    torch_linear_to_jax_linear,
+    torch_conv_to_jax_conv
+)
 
 
 def convert_patchembed(jax_layer: nnx.Module, torch_layer: nn.Module):
@@ -143,28 +87,10 @@ def convert_dinov2(jax_model: DinoViT, torch_model):
     return jax_model
 
 
-jax_dinov2 = convert_dinov2(dino_model, torch_model)
+# jax_dinov2 = convert_dinov2(dino_model, torch_model)
 
 
 # saving utils
-import flax.traverse_util, pickle
-from flax.serialization import to_state_dict, from_state_dict
-from flax.core import freeze, unfreeze
-
-
-def save_modelpickle(model, filename="model.pkl"):
-    params = nnx.state(model)
-    params = jax.device_get(params)
-
-    state_dict = to_state_dict(params)
-    frozen_state_dict = freeze(state_dict)
-
-    flat_state_dict = flax.traverse_util.flatten_dict(frozen_state_dict, sep=".")
-
-    with open(filename, "wb") as f:
-        pickle.dump(frozen_state_dict, f)
-
-    return flat_state_dict
 
 from jax import random as jrand
 import optax
